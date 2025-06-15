031c5be041a53e4170b48b4f9f34d59558389229
Ali Turgut Bozkurt <turgut.bozkurt@bilgeadam.com>
2025-06-15 15:02:43 +0300

    webapp-fe footer translation

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -41,7 +41,7 @@ export default function Footer() {
               <span className="ml-2 text-lg font-bold text-gray-800 dark:text-white">LookforX</span>
             </Link>
             <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
-              © {t('footer.copyright', { year: new Date().getFullYear().toString() })}. {t('footer.rights')}
+              © {new Date().getFullYear()} LookforX. {t('footer.rights')}
             </p>
           </div>
           
